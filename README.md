419f42a99ec4d2c017699982819ce1974f21532f
pablotoledo <jptgjuanpablo@gmail.com>
2022-02-10 12:51:13 +0100

    10.2

---
+++ README.md
@@ -0,0 +1,20 @@
+# GitHub Utils API
+
+An API library to comunicate with some admin points of the Github API not covered by PyGithub
+
+## How to use this plugin
+
+ 1. Install this library using pip `pip install github-utils-api`
+ 2. Import in your code `from githubutilsapi import GithubUtilsApi`
+ 3. Create and automate!
+
+## How to contribute
+ 1. Fork this project
+ 2. Install required software Python3
+ 3. Make your changes
+ 4. Remeber to generate documented methods
+ 5. Test your changes and create the pull request
+
+## License
+This work is licensed under the terms of the MIT license.
+For a copy, see <https://opensource.org/licenses/MIT>.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="github-utils-api",                 # This is the name of the package
+    version="0.0.4",                        # The initial release version
+    author="Pablo Toledo",                  # Full name of the author
+    description="Small library to cover non provided methods by pygithub",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["githubutilsapi"],        # Name of the python package
+    package_dir={'':'github-utils-api/src'},# Directory of the source code of the package
+    install_requires=['requests']           # Install other dependencies if any
+)
